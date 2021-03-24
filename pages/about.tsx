9a448e4f5e959fda83fff3bb4bc5cf14ff3dfc77
SirHectorin <black.shot@live.cl>
2021-03-24 14:52:31 -0300

    Primer commit

---
+++ pages/about.tsx
@@ -0,0 +1,5 @@
+export default function About(){
+    return (<div>
+        <h1>Saludos desde About!</h1>
+    </div>);
+}
---
+++ pages/contact.tsx
@@ -0,0 +1,5 @@
+export default function Contact(){
+    return (<div>
+        <h1>Saludos desde contacto!</h1>
+    </div>);
+}
